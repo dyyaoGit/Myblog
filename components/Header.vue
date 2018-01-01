95357572411095829f2004fc9626b7fd4b875c55
dyyaoGit <646617513@qq.com>
2018-01-02 02:01:52 +0800

    正在写入头部导航

---
+++ components/Header.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="nav navbar-fixed-top">
+      <div class="header clear-fixed">
+        <div class="header-left fll">
+            姚君荣的个人博客
+        </div>
+        <div class="header-center">
+            <Menu mode="horizontal" active-name="1">
+                <MenuItem name="1">
+                    <nuxt-link to="/" class="nav-item">
+                        博客首页
+                    </nuxt-link>      
+                </MenuItem>
+            </Menu>
+        </div>
+      </div>
+  </div>
+</template>
+
+<style>
+.nav .ivu-menu-horizontal {
+    line-height: 56px;
+}
+.ivu-menu-horizontal.ivu-menu-light:after {
+    content: '';
+    height: 0;
+}
+</style>
+
+<style scoped>
+.nav {
+    background-color: #fff;
+    border-bottom: 1px solid #f0f0f0;
+}
+.nav-item {
+    display: block;
+    width: 100%;
+    line-height: 56px;
+}
+.navbar-fixed-top {
+    position: fixed;
+    right: 0;
+    left: 0;
+    z-index: 1030;
+}
+.header {
+    min-width: 768px;
+    max-width: 1440px;
+    margin: 0 auto;
+    height: 56px;
+}
+.header-left {
+    line-height: 56px;
+    font-size: 20px;
+}
+.header-center {
+    width: 945px;
+    margin: 0 auto;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,7 @@
 <template>
   <section class="container">
-    <img src="~assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
-    <h1 class="title">
-      <Button @click="jump">跳转到文章页面</Button>
-    </h1>
-    <ul class="users">
-      <li v-for="(user, index) in users" :key="index" class="user">
-        <nuxt-link :to="{ name: 'id', params: { id: index }}">
-          {{ user.name }}
-        </nuxt-link>
-      </li>
-    </ul>
+    我的博客主页
+    <nuxt-link to="content">文章页</nuxt-link>
   </section>
 </template>
 
@@ -24,7 +15,7 @@ export default {
   },
   head () {
     return {
-      title: 'Users'
+      title: '姚君荣的博客'
     }
   },
   methods: {
@@ -36,18 +27,5 @@ export default {
 </script>
 
 <style scoped>
-.title
-{
-  margin: 30px 0;
-}
-.users
-{
-  list-style: none;
-  margin: 0;
-  padding: 0;
-}
-.user
-{
-  margin: 10px 0;
-}
+
 </style>
